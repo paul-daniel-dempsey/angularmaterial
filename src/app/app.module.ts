2a5e7cfe036f28b7e9be3ed65528cb8163f73380
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2021-08-21 20:05:31 +0100

    LayoutComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material.module';
 
+
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'contactmanager', loadChildren: () => import('./contactmanager/contactmanager.module').then(m => m.ContactmanagerModule)},
+  { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)},
+  { path: '**', redirectTo : 'contactmanager'}
+];
 
 @NgModule({
   declarations: [
@@ -13,9 +21,9 @@ import { MaterialModule } from './shared/material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
